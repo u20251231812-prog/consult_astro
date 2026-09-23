93e08215143f6c47b48ac77610673bf07ca0a6fc
David Felipe Trujillo Perdomo <u20251231812@usco.edu.co>
2026-09-23 09:47:10 -0500

    act site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://David22-f.github.io', // Tu usuario de GitHub
+  base: '/p1',                          // Nombre de tu repositorio
   vite: {
     plugins: [tailwindcss()]
   }
